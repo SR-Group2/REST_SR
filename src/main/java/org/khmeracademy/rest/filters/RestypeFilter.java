cabd2ba5f0586533fdeb67ad9b5f29cfde3a5f8e
hiemseyha <hiemseyha168@gmail.com>
2016-08-07 11:39:36 +0700

    add filter

---
+++ src/main/java/org/khmeracademy/rest/filters/RestypeFilter.java
@@ -0,0 +1,16 @@
+package org.khmeracademy.rest.filters;
+
+public class RestypeFilter {
+	private String keyword;
+	
+	public RestypeFilter(){
+		keyword = "";
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+}
