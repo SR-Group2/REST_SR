ff477fd273c25e874748d90f9e05ca472a0e95a4
hiemseyha <hiemseyha168@gmail.com>
2016-08-15 08:11:52 +0700

    Upload Image by seyha

---
+++ src/main/java/org/khmeracademy/rest/entities/GalleryInputUpdate.java
@@ -0,0 +1,26 @@
+package org.khmeracademy.rest.entities;
+
+public class GalleryInputUpdate {
+	private int image_id;
+	private int product_id;
+	private String image_path;
+	public int getImage_id() {
+		return image_id;
+	}
+	public void setImage_id(int image_id) {
+		this.image_id = image_id;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getImage_path() {
+		return image_path;
+	}
+	public void setImage_path(String image_path) {
+		this.image_path = image_path;
+	}
+		
+}
